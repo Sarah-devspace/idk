73e0fd5fcb52f2730a1f803c7bfe474c149726e8
Sarah-devspace <saragedamu292@gmail.com>
2026-07-01 17:17:28 +0300

    Add beginner's thoughts to mal.py

--- mal.py
+++ mal.py
@@ -1,1 +1,3 @@
 malkesm yshalal idk dude this is crazy like imagine having friends and they see me and cuz i am beginner
+you may think am playing but honeslty i am just doing my best that's it
+like fr fr fr am real one
